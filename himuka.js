2728e8fecc862a75d10cdcff12f4cd4d5178fbd4
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-10-18 02:24:55 +0900

    Update himuka.js

--- himuka.js
+++ himuka.js
@@ -17,6 +17,30 @@ setInterval(rotateHeader, 300);
 //300ミリ秒ごとにrotateHeader関数を実行する
 
 
+
+let isPositive = true;
+let opacity = 0; // 不透明度
+
+function opacityAnimate() {
+  if (isPositive) {
+    opacity += 0.05;
+  } else {
+    opacity -= 0.05;
+  }
+
+  if (opacity > 1) {
+    isPositive = false;
+  } else if (opacity < 0) {
+    isPositive = true;
+  }
+ header.style.opacity = opacity;
+}
+// 300ミリ秒ごとに opacityAnimate 関数を実行する
+setInterval(opacityAnimate, 300);
+
+
+
+
 //回答欄とその下部分
 'use strict';
 const answerInput = document.getElementById('answer');
