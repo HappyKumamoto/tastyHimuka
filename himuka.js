0c7212955b982eb870da223d94d605bdac55a93e
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-10-17 16:26:40 +0900

    Add files via upload

---
+++ himuka.js
@@ -0,0 +1,52 @@
+let header = document.getElementById('header');
+let degree = 0; //角度
+
+function rotateHeader() {
+    degree += 6  ;
+    degree = degree % 360;
+ if ( ( 90<=degree && degree <270) ) {
+      header.className = 'face';  //表面のclassを設定
+  } else {
+      header.className = 'back'; //裏面のclassを設定
+  }
+  //3Dで動かす
+  header.style.transform = 'rotate3D(1,1,1,'+ degree+'deg)';
+}
+setInterval(rotateHeader, 150);
+//ゆったり回る感じ
+//150ミリ秒ごとにrotateHeader関数を実行する
+
+
+//回答欄とその下部分
+'use strict';
+const answerInput = document.getElementById('answer');
+const assessmentButton =    document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+
+assessmentButton.onclick = ( ) => {
+const answer = answerInput.value;
+
+     if(answer.length===0) { //空欄の時は処理を終了
+        return;
+    };
+    
+   if(answer==='みやざき') {   //正解の時のみ、応募フォームを表示する
+        document.getElementById('result-area').innerHTML=(`大正解！　よろしければ
+        <br>九州 みやざきの
+        <span style="color:rgb(206, 255, 71); font-size:160%;">名産品と名所</span>
+        についての<br>アンケートにお答えください。<br>
+        <a href = "https://www.nnn.ed.nico/" target="_blank">アンケートはこちら</a>`);
+        //<a href = "https://secret-shore-96743.herokuapp.com/enquetes/tasty-red" target="_blank">
+        //アンケートはこちら(Heroku)</a> </p>`);
+       }else{
+        document.getElementById('result-area').innerHTML=`おしいです！<br>
+        再度挑戦してみてください！`;
+    };
+ };
+ 
+ //エンターキーでも機能する
+  /*answerInput.onkeydown = event => {
+    if (event.keyCode === 13) {
+      assessmentButton.onclick();
+    }
+  };*/
